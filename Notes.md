e6d0bf00bb1417a76ac05c72a1a40a81c24d7159
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-03-23 00:23:13 -0300

    feat: create cli interface with blessed lib

---
+++ Notes.md
@@ -0,0 +1,11 @@
+https://github.com/ErickWendel/semana-js-expert30
+https://github.com/pedrorenan/js-expert-week
+
+npm init -y
+node index.js
+
+https://www.npmjs.com/package/blessed
+
+npm install blessed@0.1.81
+
+npm start
---
+++ src/terminalController.js
@@ -0,0 +1,107 @@
+import ComponentsBuilder from "./components.js";
+import { constants } from "./constants.js";
+
+export default class TerminalController {
+    #usersColors = new Map();
+    
+    constructor() {}
+
+    #pickColor() {
+        return '#' + ((1 << 24) * Math.random() | 0).toString(16) + '-fg';
+    }
+
+    #getUserColor(userName) {
+        if (this.#usersColors.has(userName)) return this.#usersColors.get(userName);
+
+        const color = this.#pickColor();
+        this.#usersColors.set(userName, color);
+
+        return color;
+    }
+
+    #onInputReceived(eventEmitter) {
+        return function () {
+            const message = this.getValue();
+            console.log(message);
+            this.clearValue();
+        };
+    }
+
+    #onMessageReceived({ screen, chat }) {
+        return msg => {
+            const { userName, message } = msg;
+            const color = this.#getUserColor(userName);
+            
+            chat.addItem(`{${color}}{bold}${userName}{/}: ${message}`);
+            
+            screen.render();
+        };
+    }
+
+    #onLogChanged({ screen, activityLog }) {
+        return msg => {
+            const [userName] = msg.split(/\s/);
+            const color = this.#getUserColor(userName);
+            
+            activityLog.addItem(`{${color}}{bold}${msg.toString()}{/}`);
+            
+            screen.render();
+        };
+    }
+    
+    #onStatusChanged({ screen, status }) {
+        return users => {
+            const { content } = status.items.shift();
+            status.clearItems();
+            status.addItem(content);
+
+            users.forEach(userName => {
+                const color = this.#getUserColor(userName);
+                status.addItem(`{${color}}{bold}${userName}{/}`);
+            });
+            
+            screen.render();
+        };
+    }
+
+    #registerEvents(eventEmmiter, components) {
+        eventEmmiter.on(constants.events.app.MESSAGE_RECEIVED, this.#onMessageReceived(components));
+        eventEmmiter.on(constants.events.app.ACTIVITY_UPDATED, this.#onLogChanged(components));
+        eventEmmiter.on(constants.events.app.STATUS_UPDATED, this.#onStatusChanged(components));
+    }
+
+    async initializaTable(eventEmmiter) {
+        const components = new ComponentsBuilder()
+            .setCreen({ title: 'HackerChat - DevOnTheRun' })
+            .setLayoutComponent()
+            .setInputComponent(this.#onInputReceived(eventEmmiter))
+            .setChatComponent()
+            .setActivityLogComponent()
+            .setStatusComponent()
+            .build();
+
+        this.#registerEvents(eventEmmiter, components);
+
+        components.input.focus();
+        components.screen.render();
+
+        /*
+        // Tests
+        setInterval(() => {
+            eventEmmiter.emit(constants.events.app.ACTIVITY_UPDATED, 'devOnTheRun join');
+            eventEmmiter.emit(constants.events.app.ACTIVITY_UPDATED, 'johnDoe join');
+            eventEmmiter.emit(constants.events.app.MESSAGE_RECEIVED, { message: 'Hello World!',  userName: 'devOnTheRun'});
+            eventEmmiter.emit(constants.events.app.MESSAGE_RECEIVED, { message: 'Hi!',  userName: 'johnDoe'});
+            eventEmmiter.emit(constants.events.app.ACTIVITY_UPDATED, 'devOnTheRun left');
+            eventEmmiter.emit(constants.events.app.ACTIVITY_UPDATED, 'johnDoe left');
+        }, 1000)
+
+        const users = ['devOnTheRun'];
+        eventEmmiter.emit(constants.events.app.STATUS_UPDATED, users);
+        users.push('johnDoe', 'bobDylan');
+        eventEmmiter.emit(constants.events.app.STATUS_UPDATED, users);
+        users.push('first', 'second');
+        eventEmmiter.emit(constants.events.app.STATUS_UPDATED, users);
+        */
+    }
+}
