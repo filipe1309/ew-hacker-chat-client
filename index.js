546dcaeb8eb3ceec84830c9077fcb3e8310f92cf
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-03-24 22:48:02 -0300

    --amend

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const socketClient = new SocketClient(config);
 await socketClient.initialize();
 
 const eventManager = new EventManager({ componentEmitter, socketClient });
+const events = eventManager.getEvents();
+socketClient.attachEvents(events);
+
 const data = {
     roomId: config.room,
     userName: config.username
