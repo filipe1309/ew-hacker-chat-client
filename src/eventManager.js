1bbfa8a3666696525ccdbc24d671740a16aecc17
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-03-24 22:12:57 -0300

    chore: add update users event

--- src/eventManager.js
+++ src/eventManager.js
@@ -1,6 +1,8 @@
 import { constants } from "./constants.js";
 
 export default class EventManager {
+    #allUsers = new Map();
+    
     constructor({ componentEmitter, socketClient }) {
         this.componentEmitter = componentEmitter
         this.socketClient = socketClient;
@@ -12,5 +14,18 @@ export default class EventManager {
         this.componentEmitter.on(constants.events.app.MESSAGE_SENT, msg => {
             this.socketClient.sendMessage(constants.events.socket.MESSAGE, msg);
         });
+    }
+
+    updateUsers(users) {
+        const connectedUsers = users;
+        connectedUsers.forEach(({ id, userName }) => this.#allUsers.set(id, userName));
+        this.#updateUsersComponent(this.#allUsers)
+    }
+
+    #updateUsersComponent(allUsers) {
+        this.componentEmitter.emit(
+            constants.events.app.STATUS_UPDATED,
+            Array.from(allUsers.values())
+        )
     }
 }
